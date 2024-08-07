34edeeb70d5ce4253507999c5012919887d274f8
macarena.verdugo <macarena.verdugo@novaquality.es>
2024-08-07 16:04:10 +0200

    Correccion duplicados

--- src/main/java/com/example/mvg_api/service/CountryService.java
+++ src/main/java/com/example/mvg_api/service/CountryService.java
@@ -18,12 +18,12 @@ public class CountryService {
     public void fetchAndStoreCountries() {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://restcountries.com/v3.1/all";
-
+        // Realiza la solicitud GET a la API externa
         Object[] response = restTemplate.getForObject(url, Object[].class);
 
         if (response != null) {
             for (Object obj : response) {
-
+                // Mapea la respuesta JSON a un objeto Java
                 @SuppressWarnings("unchecked")
                 Map<String, Object> countryMap = (Map<String, Object>) obj;
 
@@ -34,11 +34,20 @@ public class CountryService {
                 // Extrae la población
                 Long population = ((Number) countryMap.get("population")).longValue();
 
-                // Crear y guardar cada uno de los registros
-                Country country = new Country();
-                country.setCommonName(commonName);
-                country.setPopulation(population);
-                countryRepository.save(country);
+                // Verifica si el país ya existe en la base de datos
+                Country existingCountry = countryRepository.findByCommonName(commonName);
+
+                if (existingCountry != null) {
+                    // Actualiza el país existente
+                    existingCountry.setPopulation(population);
+                    countryRepository.save(existingCountry);
+                } else {
+                    // Crea una nueva instancia de la entidad Country y la guarda en la base de datos
+                    Country country = new Country();
+                    country.setCommonName(commonName);
+                    country.setPopulation(population);
+                    countryRepository.save(country);
+                }
             }
         }
     }
